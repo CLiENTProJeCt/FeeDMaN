23e1b4923d984919578b1ddf641f7dc73d1602cf
CLiENT ProJeCtS <CLiTEaMTG@GMail.com>
2016-09-26 15:37:59 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -215,6 +215,15 @@ function create_config( )
     "banhammer",
     "stats",
     "anti_spam",
+    "addplug",
+    "plugins",
+    "supergroup",
+    "whitelist",
+    "lock_bot",
+    "lock_fwd",
+    "expire",
+    "delgb",
+    "badword",
     "owners",
     "arabic_lock",
     "set",
@@ -226,30 +235,30 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {264465600,118208388,120741730,264762805},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
+    about_text = [[FeeDMaN v2 - Open Source
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
+https://github.com/----/----
 
 Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
+EsmaIL (@CLiENT)
+Mahdi cyb3r (@mahdi_killer78)
+amir ramezani (@antispam_sudo)
+ninja (@ninjahacker)
+AF8 (@AF8_1)
 
 Special thanks to:
-Juan Potato
-Siyanew
-Topkecleon
-Vamptacus
+YAgop
+Mahdi cyb3r
+Seed TEam
+server pars
+globalkey
 
 Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
+persian: @CLiENT_ProJeCts
 ]],
     help_text_realm = [[
 Realm Commands:
